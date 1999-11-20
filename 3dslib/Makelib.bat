f4ed33d54fdb398e3aaeff081ce234a35454812f
gereoffy <arpi.git@interoot.hu>
1999-11-20 06:20:32 +0000

    add Makefile

---
+++ 3dslib/Makelib.bat
@@ -0,0 +1,10 @@
+wcc386 -otexan -s -fp5 -fpi87 -5 ast3d.c
+wlib ast3d.lib +-ast3d.obj
+wcc386 -otexan -s -fp5 -fpi87 -5 camera.c
+wlib ast3d.lib +-camera.obj
+wcc386 -otexan -s -fp5 -fpi87 -5 file_3ds.c
+wlib ast3d.lib +-file_3ds.obj
+wcc386 -otexan -s -fp5 -fpi87 -5 quatern.c
+wlib ast3d.lib +-quatern.obj
+wcc386 -otexan -s -fp5 -fpi87 -5 spline.c
+wlib ast3d.lib +-spline.obj
